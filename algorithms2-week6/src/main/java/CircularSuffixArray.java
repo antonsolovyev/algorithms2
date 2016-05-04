da47914918a3f8087bc3132bc97306033f6aece8
Anton Solovyev <anton@solovyev.com>
2016-05-03 23:56:12 -0600

    Speed up CircularSuffixArray

--- algorithms2-week6/src/main/java/CircularSuffixArray.java
+++ algorithms2-week6/src/main/java/CircularSuffixArray.java
@@ -3,24 +3,17 @@ import java.util.Comparator;
 
 public class CircularSuffixArray {
     private String string;
-    private Integer[] indexes;
+    private int[] indexes;
     
     public CircularSuffixArray(String string) {
         this.string = string;
         
-        indexes = new Integer[string.length()];
+        indexes = new int[string.length()];
         for(int i = 0; i < indexes.length; i++) {
             indexes[i] = i;
         }
 
-        Arrays.sort(indexes, new Comparator<Integer>() {
-            @Override
-            public int compare(Integer i1, Integer i2) {
-                String s1 = new StringBuilder(string.substring(i1)).append(string.substring(0, i1)).toString();
-                String s2 = new StringBuilder(string.substring(i2)).append(string.substring(0, i2)).toString();
-                return s1.compareTo(s2);
-            }
-        });
+        sort(indexes);
     } // circular suffix array of s
     
     public int length() {
@@ -30,4 +23,47 @@ public class CircularSuffixArray {
     public int index(int i) {
         return indexes[i];
     }              // returns index of ith sorted suffix
+
+    private void sort(int[] a) {
+        sort(a, 0, a.length - 1, 0);
+    }
+
+    private void sort(int[] a, int lo, int hi, int d) {
+        if(hi <= lo) {
+            return;
+        }
+        
+        int lt = lo, gt = hi;
+        int v = charAt(a[lo], d);
+        int i = lo + 1;
+        while (i <= gt) {
+            int t = charAt(a[i], d);
+            if(t < v) {
+                exch(a, lt++, i++);
+            } else if (t > v) {
+                exch(a, i, gt--);
+            } else {
+                i++;
+            }
+        }
+
+        sort(a, lo, lt - 1, d);
+        if (v >= 0) {
+            sort(a, lt, gt, d + 1);
+        }
+        sort(a, gt + 1, hi, d);
+    }
+
+    private int charAt(int i, int d) {
+        if (d == string.length()) {
+            return -1;
+        }
+        return string.charAt((d + i) % string.length());
+    }
+    
+    private void exch(int[] a, int i, int j) {
+        int temp = a[i];
+        a[i] = a[j];
+        a[j] = temp;
+    }
 }
